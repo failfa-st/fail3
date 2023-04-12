db5b65a27da533cd8731bce75a116cc249909571
TimPietrusky <timpietrusky@gmail.com>
2023-04-12 16:27:29 +0200

    feat: universal starter for fail3 and dev

--- dev.js
+++ dev.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 /**
+ * This script runs the TypeScript file located at `src/index.ts` using `ts-node-esm`.
+ * It takes any additional command-line arguments and passes them to the TypeScript file.
+ *
  * It first imports necessary modules such as `node:path`, `node:process`, and `node:url`.
  * It then uses `url.fileURLToPath` to convert the `import.meta.url` to a path to the current
  * directory.
@@ -33,14 +36,16 @@ config();
 // Use `url.fileURLToPath` to convert the `import.meta.url` to a path to the current directory
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-// Join the directory path with entry to obtain the full path to the TypeScript file
-const entry = path.join(__dirname, "src/index.ts");
+// Join the directory path with `src/index.ts` to obtain the full path to the TypeScript file
+const index = path.join(__dirname, "src/dev.ts");
 
 // Extract any additional command-line arguments and pass them to the TypeScript file
 const argv = process.argv.splice(2);
 
+// Execute `npx ts-node-esm src/index.ts ...args` command with the current directory as working
+// directory
 // Pass any output produced by the TypeScript file to the standard output stream
-await execa("npx", ["ts-node-esm", entry, ...argv], {
+await execa("npx", ["ts-node-esm", index, ...argv], {
 	stdio: "inherit",
 	cwd: __dirname,
 });
